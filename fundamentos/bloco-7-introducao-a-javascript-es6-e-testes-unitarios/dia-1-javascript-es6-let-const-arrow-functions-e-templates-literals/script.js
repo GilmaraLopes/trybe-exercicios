fce82e1576d563af538e097df51b098fab683693
Gilmara <gilmaralopes2006@gmail.com>
2022-08-15 09:18:07 -0300

    atividade 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-templates-literals/script.js
@@ -1,14 +1,25 @@
-const testingScope = (escopo) => {
+// const testingScope = (escopo) => {
 
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
-  
-}
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
 
-testingScope(false);
+// }
+
+// testingScope(false);
+
+
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// oddsAndEvens.sort((a, b) => a - b);
+
+// console.log(`Os numeros ${ oddsAndEvens } se encontram ordenados de forma crescente!`);
+
+
+
